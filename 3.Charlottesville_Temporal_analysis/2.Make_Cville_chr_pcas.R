3592266ec514c7fcc50b700f0509a1645d38f5c3
Joaquin Nunez <jcbnunez@d-172-25-129-134.dhcp.virginia.edu>
2021-10-07 14:32:42 -0400

    fix cross 2 - 3

--- 3.Charlottesville_Temporal_analysis/2.Make_Cville_chr_pcas.R
+++ 3.Charlottesville_Temporal_analysis/2.Make_Cville_chr_pcas.R
@@ -99,129 +99,19 @@ ggsave(cville_chr_pca,
        width = 5,
        height = 4)
 
-
-#### Part 2 run regressions against various predictors
-
-pca_table_df$chr %>% unique -> chrs_vec
-
-regressions_list1 = list()
-regressions_list2 = list()
-regressions_list3 = list()
-
-for(i in 1:length(chrs_vec)){
-  
-  print(chrs_vec[i])
-  
-lm(Dim.1 ~ year + 
-           MeanEC + 
-           Month + 
-           `In(2L)t`  +  
-           `In(2R)Ns`  + 
-           `In(3L)P` +
-           `In(3R)C` +   
-           `In(3R)Mo`+ 
-           `In(3R)Payne`, 
-   data = pca_table_df[which(pca_table_df$chr == chrs_vec[i]),] ) -> 
-  lm_out
-
-  summary(lm_out) -> lm_summ
-  
-  lm_summ$coefficients %>%
-    as.data.frame() %>%
-    mutate(variable = rownames(.),
-           chr = chrs_vec[i],
-           PC = 1,
-           signif = ifelse(.$`Pr(>|t|)` < 0.05, "S", "NS")) ->
-    lm_sum_fin
-
-  regressions_list1[[i]] = lm_sum_fin
-  
-  #######
-
-  lm(Dim.2 ~ year + 
-       MeanEC + 
-       Month + 
-       `In(2L)t`  +  
-       `In(2R)Ns`  + 
-       `In(3L)P` +
-       `In(3R)C` +   
-       `In(3R)Mo`+ 
-       `In(3R)Payne`, 
-     data = pca_table_df[which(pca_table_df$chr == chrs_vec[i]),] ) -> 
-    lm_out
-  
-  summary(lm_out) -> lm_summ
-  
-  lm_summ$coefficients %>%
-    as.data.frame() %>%
-    mutate(variable = rownames(.),
-           chr = chrs_vec[i],
-           PC = 2,
-           signif = ifelse(.$`Pr(>|t|)` < 0.05, "S", "NS")) ->
-    lm_sum_fin
-  
-  regressions_list2[[i]] = lm_sum_fin
-  
-  #####
-  
-  lm(Dim.3 ~ year + 
-       MeanEC + 
-       Month + 
-       `In(2L)t`  +  
-       `In(2R)Ns`  + 
-       `In(3L)P` +
-       `In(3R)C` +   
-       `In(3R)Mo`+ 
-       `In(3R)Payne`, 
-     data = pca_table_df[which(pca_table_df$chr == chrs_vec[i]),] ) -> 
-    lm_out
-  
-  summary(lm_out) -> lm_summ
-  
-  lm_summ$coefficients %>%
-    as.data.frame() %>%
-    mutate(variable = rownames(.),
-           chr = chrs_vec[i],
-           PC = 3,
-           signif = ifelse(.$`Pr(>|t|)` < 0.05, "S", "NS")) ->
-    lm_sum_fin
-  
-  regressions_list3[[i]] = lm_sum_fin
-  
-}
-
-regressions_df <-
-rbind(do.call(rbind, regressions_list1),
-      do.call(rbind, regressions_list2),
-      do.call(rbind, regressions_list3))
-
-write.table(regressions_df, file = "./regression_cville_pcs.csv", append = FALSE, quote = FALSE, sep = ",",
-            eol = "\n", na = "NA", dec = ".", row.names = FALSE,
-            col.names = TRUE, qmethod = c("escape", "double"),
-            fileEncoding = "")
-
-
-regressions_df %>% 
-  filter(signif == "S",
-         variable != "(Intercept)") %>% 
-  select(chr, PC, variable, `Pr(>|t|)`) %>%
-  dcast(PC+chr~variable)
-
-
-##### Month Analysis
-
 ##########
 ### plot specific panels
 #####
 
+## inv 2LT
 pca_table_df %>%
-  filter(chr == "2L") %>%
   ggplot(aes(
     x=`In(2L)t`,
-    y=Dim.1
+    y=Dim.1,
+    color = chr
   )) + 
-  geom_point(size = 3) +
-  geom_smooth(method = "lm") ->
+  geom_smooth(method = "lm", se = F) +
+  geom_point(size = 2)  ->
   inv_vs_pc1
 
 ggsave(inv_vs_pc1,
@@ -229,17 +119,15 @@ ggsave(inv_vs_pc1,
        width = 4,
        height = 4)
 
-## Plot variance pannel
-
+## year signal
 pca_table_df %>%
-  filter(chr != "2L") %>%
   ggplot(aes(
     x=year,
-    y=Dim.2
+    y=Dim.2,
+    color = chr
   )) + 
-  geom_point(size = 3) +
-  geom_smooth(method = "lm") +
-  facet_wrap(~chr, ncol =2) ->
+  geom_smooth(method = "lm", se = F) +
+  geom_point(size = 2)  ->
   year_vs_pc2
 
 ggsave(year_vs_pc2,
@@ -247,30 +135,186 @@ ggsave(year_vs_pc2,
        width = 4,
        height = 4)
 
-### projection panel
-
+## year signal
 pca_table_df %>%
   ggplot(aes(
     x=as.factor(month_col),
-    y=abs(Dim.1)
-  )) +
-  geom_boxplot() +
-  geom_point() ->
-  month_proejctions
+    y=abs(Dim.1),
+    fill = chr
+  )) + 
+  geom_boxplot()  ->
+  month_vs_pc1
 
-ggsave(month_proejctions,
-       file = "month_proejctions.pdf")
+ggsave(
+inv_vs_pc1/
+year_vs_pc2/
+month_vs_pc1,
+       file = "pannel_figures.pdf",
+       width = 4,
+       height = 9)
 
-pca_table_df %>%
-  ggplot(aes(
-    x=as.factor(month_col),
-    y=abs(Dim.2)
-  )) +
-  geom_boxplot() +
-  geom_point() +
-  facet_wrap(~chr)->
-  month_proejction2
+##### RUN REGRESSIONS
 
-ggsave(month_proejction2,
-       file = "month_proejction2.pdf")
+###Correlations with 2lt? -- begin
+PCA_object$ind$coord %>%
+  as.data.frame() %>%
+  mutate(sampleId = rownames(.),
+         Run = chr) %>% 
+  left_join(., filtered_samps_for_analysis ) -> PCA_table
+
+dims_captured=dim(PCA_object$ind$coord)[2]
+
+PCA_table$year = as.numeric(PCA_table$year)
+
+### Save 
+save(PCA_table, file = paste(chr, "pca_coords", "Rdata", sep = "."))
+
+#### Run linear regressions
+#### 
+guide=paste("Dim", 1:dims_captured, sep = ".")
+Dim_lm_list = list()
+
+PCA_table %>%
+  .[,c(grep("Dim", colnames(.)),
+       grep("year", colnames(.)))] %>%
+  melt(id = "year") ->
+  collection_year_df
+
+PCA_model_fit = collection_year_df
+type="year"
+
+for(i in 1:dims_captured){
+  print(i)
+  
+  lm(data = PCA_model_fit[which(PCA_model_fit$variable == guide[i] ),],
+     value ~ year ) -> tmp
+  
+  summary(tmp) %>%
+    .$coefficients %>%
+    as.data.frame() %>%
+    mutate(Dim = guide[i], 
+           terms = rownames(.),
+           run_type= chr,
+           test_type = type
+    ) -> tmp_s
+  
+  Dim_lm_list[[i]] = tmp_s[grep("(Intercept)", rownames(tmp_s), invert = T),]
+  
+}
+
+Dim_lm_time = do.call(rbind, Dim_lm_list)
+Dim_lm_time %<>%
+  mutate(p.val.fdr = p.adjust(`Pr(>|t|)`, method = "fdr"),
+         p.val.bonfe = p.adjust(`Pr(>|t|)`, method = "bonferroni"))
+
+############# Inversions
+############# 
+
+inversions =c(
+  "In(2L)t",
+  "In(2R)Ns",
+  "In(3L)P",
+  "In(3R)C",
+  "In(3R)Mo",
+  "In(3R)Payne")
+
+inversions = gsub("[\\(\\)]", "_", inversions)
+names(PCA_table) = gsub("[\\(\\)]", "_", names(PCA_table))
+
+
+PCA_table %>%
+  .[,c(grep("Dim", colnames(.)),which(names(.) %in% inversions))] %>%
+  melt(id = inversions) ->
+  in_df
+
+
+guide=paste("Dim", 1:dims_captured, sep = ".")
+Dim_lm_list = list()
+PCA_model_fit = in_df
+type="inv"
+for(i in 1:dims_captured){
+  print(i)
+  
+  
+  varlist <- inversions
+  
+  models <- lapply(varlist, function(x) {
+    lm(substitute(value ~ i, list(i = as.name(x))), 
+       data = PCA_model_fit[which(PCA_model_fit$variable == guide[i] ),])
+  })
+  
+  lapply(models, summary) %>%
+    lapply(., coef) %>%
+    do.call(rbind, .) %>%
+    .[grep("(Intercept)", rownames(.), invert = T),] %>%
+    as.data.frame() %>%
+    mutate(Dim = guide[i], 
+           terms = rownames(.),
+           run_type= chr,
+           test_type = type) -> tmp_s
+  
+  Dim_lm_list[[i]] = tmp_s[]
+  
+}
+
+Dim_lm_inv = do.call(rbind, Dim_lm_list)
+Dim_lm_inv %<>%
+  mutate(p.val.fdr = p.adjust(`Pr(>|t|)`, method = "fdr"),
+         p.val.bonfe = p.adjust(`Pr(>|t|)`, method = "bonferroni"))
+
+############# Effective Coverage
+############# 
+
+PCA_table %>%
+  .[,c(grep("Dim", colnames(.)),
+       grep("MeanEC", colnames(.)))] %>%
+  melt(id = "MeanEC") ->
+  MeanEC_year_df
+
+guide=paste("Dim", 1:dims_captured, sep = ".")
+Dim_lm_list = list()
+PCA_model_fit = MeanEC_year_df
+type="EC"
+for(i in 1:dims_captured){
+  print(i)
+  
+  lm(data = PCA_model_fit[which(PCA_model_fit$variable == guide[i] ),],
+     value ~ MeanEC ) -> tmp
+  
+  summary(tmp) %>%
+    .$coefficients %>%
+    as.data.frame() %>%
+    mutate(Dim = guide[i], 
+           terms = rownames(.),
+           run_type= chr,
+           test_type = type
+    ) -> tmp_s
+  
+  Dim_lm_list[[i]] = tmp_s[grep("(Intercept)", rownames(tmp_s), invert = T),]
+  
+}
+
+Dim_lm_EC = do.call(rbind, Dim_lm_list)
+Dim_lm_EC %<>%
+  mutate(p.val.fdr = p.adjust(`Pr(>|t|)`, method = "fdr"),
+         p.val.bonfe = p.adjust(`Pr(>|t|)`, method = "bonferroni"))
+
+#### save output
+#### 
+
+rbind(
+  Dim_lm_time,
+  Dim_lm_inv,
+  Dim_lm_EC
+) -> merged_lm_output
+
+write.table(merged_lm_output, 
+            file = paste(chr, "lm_fits", "txt" , sep = ".") ,
+            append = FALSE, 
+            quote = FALSE, 
+            sep = "\t",
+            na = "NA", 
+            dec = ".", 
+            row.names = FALSE,
+            col.names = TRUE)
 
